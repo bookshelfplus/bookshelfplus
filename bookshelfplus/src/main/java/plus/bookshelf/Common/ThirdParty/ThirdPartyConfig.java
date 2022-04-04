3af1b80d2a43c5c7c3f79818d8b8e8d1caaf0010
程序员小墨 <2291200076@qq.com>
2022-04-04 10:55:15 +0800

    修改配置文件加载方式

--- bookshelfplus/src/main/java/plus/bookshelf/Common/ThirdParty/ThirdPartyConfig.java
+++ bookshelfplus/src/main/java/plus/bookshelf/Common/ThirdParty/ThirdPartyConfig.java
@@ -2,13 +2,9 @@ package plus.bookshelf.Common.ThirdParty;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "thirdparty")
-@PropertySource("thirdparty.properties")
 @Data
 public class ThirdPartyConfig {
 
