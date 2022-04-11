9a19a99fd6354ee833bc1dcce72c886f5032a062
程序员小墨 <2291200076@qq.com>
2022-04-11 22:50:29 +0800

    第三方登录回调跳转地址错误修正，用户收藏功能异常修复

--- bookshelfplus/使用D盘temp file目录下依赖运行.bat
+++ bookshelfplus/使用D盘temp file目录下依赖运行.bat
@@ -1,1 +1,5 @@
-java -Dloader.path=D:\temp\lib -jar target/bookshelfplus-1.0-SNAPSHOT.jar
+java -Dloader.path=D:\temp\lib -Ddruid.mysql.usePingMethod=false -jar target/bookshelfplus-1.0-SNAPSHOT.jar
+
+
+:: 2022-04-11 11:27:02.240  WARN 15012 --- [nio-8090-exec-3] c.a.druid.pool.DruidAbstractDataSource   : discard long time none received connection. , jdbcUrl : jdbc:mysql://127.0.0.1:3306/bookshelfplus?useSSL=false&serverTimezone=Asia/Shanghai, version : 1.2.8, lastPacketReceivedIdleMillis : 70565
+:: 解决方法：-Ddruid.mysql.usePingMethod=false
